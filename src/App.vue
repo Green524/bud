409ab268699f57fbd702f2b0c21fe9a4fd0623d5
陈 骠骑 <chenpiaoqi@local.lan>
2022-08-08 15:27:39 +0800

    ff

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,68 @@
 <template>
-    <RootBody></RootBody>
+  <el-container>
+    <el-aside width="200px">
+      <el-row class="tac">
+        <el-col>
+          <el-menu
+              class="el-menu-vertical-demo"
+              default-active="1"
+              background-color="#fff"
+              text-color="#000"
+              active-text-color="rgb(97, 155, 111)"
+              >
+            <el-menu-item index="1">
+              <i class="el-icon-s-home"></i>
+            </el-menu-item>
+            <el-menu-item index="2">
+              <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <span slot="title">导航三</span>
+            </el-menu-item>
+            <el-menu-item index="5">
+              <span slot="title">导航四</span>
+            </el-menu-item>
+          </el-menu>
+        </el-col>
+      </el-row>
+    </el-aside>
+    <el-main>Main</el-main>
+  </el-container>
 </template>
-
 <script>
-import RootBody from "@/components/RootBody";
 export default {
   name: 'App',
-  components:{
-    RootBody
-  }
 }
 </script>
 
 <style scoped>
+.el-aside {
+  background-color: #fff;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+  width: 50px;
+  font-family: fantasy;
+}
 
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+  height: 100%;
+}
+.el-submenu__title:hover{
+  background-color: rgb(97, 155, 111) !important;
+}
+.el-menu-item:hover {
+  background-color: rgb(139, 183, 151) !important;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import ElementUI from 'element-ui';
-import {Button,Select} from "element-ui";
+import ElementUI from "element-ui";
 
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/css/global.css'
 
 Vue.config.productionTip = false
-// Vue.use(ElementUI);
-Vue.use(Button,Select);
-// Vue.use(Select);
-
+Vue.use(ElementUI);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
