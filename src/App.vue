aa71376cfec9e0831396bb99de0a0fb6d0994703
LinZF <c17680388727@163.com>
2022-08-29 08:55:53 +0800

    vue resources

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,51 @@
 <template>
   <div>
-  <el-container>
-    <el-aside width="13%">
-      <el-row class="tac">
-        <el-col>
-          <el-menu
-              class="el-menu-vertical-demo"
-              :default-active="`/article/latest`"
-              default-openeds=""
-              background-color="#E9EEF3"
-              text-color="#000"
-              active-text-color="rgb(97, 155, 111)"
-              :router="true"
-          >
-            <el-menu-item index="/article/latest">
-              <span slot="title">最新文章</span>
-            </el-menu-item>
-            <el-menu-item index="/article/archive">
-              <span slot="title">文章存档</span>
-            </el-menu-item>
-            <el-menu-item index="/about">
-              <span slot="title">关于我</span>
-            </el-menu-item>
-          </el-menu>
-        </el-col>
-      </el-row>
-    </el-aside>
-    <el-main class="goTop">
-      <router-view>
-      </router-view>
-      <template>
-        <el-backtop target=".goTop" :right="40" :bottom="50"></el-backtop>
-      </template>
-      <template>
-          <Pagination></Pagination>
-      </template>
-    </el-main>
-  </el-container>
+    <el-container>
+      <el-aside width="13%">
+        <el-row class="tac">
+          <el-col>
+            <el-menu
+                class="el-menu-vertical-demo"
+                :default-active="`/article/latest`"
+                background-color="#E9EEF3"
+                text-color="#000"
+                active-text-color="rgb(97, 155, 111)"
+                :router="true"
+            >
+              <el-menu-item index="/article/latest">
+                <span slot="title">最新文章</span>
+              </el-menu-item>
+              <el-menu-item index="/article/archive">
+                <span slot="title">文章存档</span>
+              </el-menu-item>
+              <el-menu-item index="/about">
+                <span slot="title">关于我</span>
+              </el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row>
+      </el-aside>
+      <el-main class="goTop">
+        <router-view>
+        </router-view>
+        <template>
+          <el-backtop target=".goTop" :right="40" :bottom="50"></el-backtop>
+        </template>
+        <template>
+          <Pagination/>
+        </template>
+      </el-main>
+    </el-container>
   </div>
 </template>
 <script>
 // import MainItemList from "@/components/MainItemList";
 // import ArticleDetail from "@/components/ArticleDetail";
 import Pagination from "@/components/Pagination";
+
 export default {
   name: 'App',
-  components:{Pagination}
+  components: {Pagination}
 }
 </script>
 
@@ -69,6 +69,7 @@ export default {
   line-height: 15px;
   overflow: visible;
 }
+
 .el-submenu__title:hover {
   background-color: rgb(97, 155, 111) !important;
 }
@@ -85,7 +86,8 @@ export default {
   height: 100vh;
   overflow-x: hidden;
 }
-.el-backtop{
+
+.el-backtop {
   color: rgb(97, 155, 111);
 }
 </style>
--- src/components/MainItemList.vue
+++ src/components/MainItemList.vue
@@ -2,24 +2,45 @@
   <section class="MainItemList-Container">
     <el-page-header @back="goBack" content="最新文章" style="margin-bottom: 30px"></el-page-header>
     <ArticleCard v-for="(item,index) in 17" :key="index"></ArticleCard>
+
+    <el-button @click="getArticlePage">点击</el-button>
   </section>
 </template>
 
 <script>
 import ArticleCard from "@/components/ArticleCard";
+
+// const getArticlePageParams = {
+//   pageNum: 1,
+//   pageSize: 1
+// }
+// function getArticlePageParams() {
+//   return {
+//     pageNum: 1,
+//     pageSize: 1
+//   };
+// }
 export default {
   name: "MainItemList",
-  components:{ArticleCard},
+  components: {ArticleCard},
   methods: {
     goBack() {
       console.log('go back');
+    },
+    getArticlePage() {
+      return this.$http.get(
+          'http://localhost:8842/blog/query/page', {
+            params:{ pageNum: 1, pageSize: 1}
+          }).then(function (success) {
+        console.log('请求成功' + success.text());
+      }, function (error) {
+        console.log('请求失败' + error.text);
+      });
     }
   }
 }
 </script>
 
 <style scoped>
-/*.el-page-header{*/
-/*  margin-bottom: 100px*/
-/*}*/
+
 </style>
