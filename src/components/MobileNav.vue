71931954dd997f8a2aa6f050226e1ecac15c0296
LinZF <c17680388727@163.com>
2022-08-30 21:59:10 +0800

    MobileNav.vue

--- src/components/MobileNav.vue
+++ src/components/MobileNav.vue
@@ -1,9 +1,10 @@
 <template>
   <nav class="MobileNav-Container">
     <van-dropdown-menu active-color="#FFF">
-      <van-icon name="wap-nav" />
-      <van-dropdown-item title="菜单" :v-model="0" :options="option1">
-
+      <van-dropdown-item title="菜单" :v-model="0" icon="1">
+        <van-cell center title="最新文章" is-link to="/article/latest/"/>
+        <van-cell center title="文章存档" is-link to="/article/archive/"/>
+        <van-cell center title="关于我" is-link to="/about/"/>
       </van-dropdown-item>
     </van-dropdown-menu>
   </nav>
@@ -12,18 +13,18 @@
 <script>
 export default {
   name: "MobileNav",
-  data(){
+  data() {
     return {
 
-      option1:[
-        { text: '最新文章', value: 0 },
-        { text: '文章存档', value: 1 },
-        { text: '关于我', value: 2 },
+      option1: [
+        {text: '最新文章', value: '/article/latest/'},
+        {text: '文章存档', value: '/article/archive/'},
+        {text: '关于我', value: '/about/'},
       ]
     }
   },
-  methods:{
-    router(){
+  methods: {
+    router() {
       this.$router.replace("/11")
     }
   }
@@ -31,10 +32,11 @@ export default {
 </script>
 
 <style scoped>
-.van-dropdown-menu{
+.van-dropdown-menu {
   float: right;
 }
-/deep/ .van-dropdown-menu__bar{
+
+/deep/ .van-dropdown-menu__bar {
   background-color: rgb(139, 183, 151) !important;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import VueRouter from "vue-router";
 import ElementUI from "element-ui";
 import router from "@/route";
 import VueResourceEsm from "vue-resource";
-import { DropdownMenu, DropdownItem,Cell } from 'vant';
+import { DropdownMenu, DropdownItem ,Cell} from 'vant';
 
 import 'element-ui/lib/theme-chalk/index.css';
 import 'vant/lib/index.css';
