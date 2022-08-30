2fccf2dd584ca0290ed0fc7ab30787eada00d7cc
陈 骠骑 <chenpiaoqi@local.lan>
2022-08-30 12:17:45 +0800

    稍微适配了移动端

---
+++ src/components/MobileNav.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="MobileNav-Container">
+    <van-dropdown-menu active-color="#FFF">
+      <van-dropdown-item title="菜单" :v-model="0" :options="option1"/>
+    </van-dropdown-menu>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "MobileNav",
+  data(){
+    return {
+      option1:[
+        { text: '最新文章', value: 0 },
+        { text: '文章存档', value: 1 },
+        { text: '关于我', value: 2 },
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.van-dropdown-menu{
+  float: right;
+}
+/deep/ .van-dropdown-menu__bar{
+  background-color: rgb(139, 183, 151) !important;
+}
+</style>
